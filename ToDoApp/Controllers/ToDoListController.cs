7451c7039cdc3779b4f85f9b83e0f545ce40aea4
Maxim Gorin <mxmgorin@gmail.com>
2019-02-03 20:18:01 +0200

    changess to views

--- ToDoApp/Controllers/ToDoListController.cs
+++ ToDoApp/Controllers/ToDoListController.cs
@@ -15,21 +15,18 @@ namespace ToDoApp.Controllers
 {
     public class ToDoListController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         private IEnumerable<ToDoItem> GetToDoList()
         {
             string currentUserId = User.Identity.GetUserId();
             ApplicationUser currentUser = db.Users.FirstOrDefault(x => x.Id == currentUserId);
-
             IEnumerable<ToDoItem> toDoList = db.ToDoList.ToList().Where(x => x.User == currentUser);
-
             int completeCount = 0;
 
             foreach (var toDoItem in toDoList)
             {
-                if (toDoItem.IsDone)
-                {
-                    completeCount++;
-                }
+                if (toDoItem.IsDone) completeCount++;
             }
 
             ViewBag.Percent = Math.Round(100f * ((float)completeCount / (float)toDoList.Count()));
@@ -43,8 +40,7 @@ namespace ToDoApp.Controllers
             return PartialView("_ToDoTable", GetToDoList());
         }
 
-        private ApplicationDbContext db = new ApplicationDbContext();
-
+        
         [Authorize]
         public ActionResult Index()
         {
@@ -57,28 +53,26 @@ namespace ToDoApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                string currentUserId = User.Identity.GetUserId();
-                ApplicationUser currentUser = db.Users.FirstOrDefault(x => x.Id == currentUserId);
-                toDoItem.User = currentUser;
-                toDoItem.IsDone = false;
-                db.ToDoList.Add(toDoItem);
-                db.SaveChanges();
+                if (!string.IsNullOrWhiteSpace(toDoItem.Description))
+                {
+                    string currentUserId = User.Identity.GetUserId();
+                    ApplicationUser currentUser = db.Users.FirstOrDefault(x => x.Id == currentUserId);
+                    toDoItem.User = currentUser;
+                    toDoItem.IsDone = false;
+                    db.ToDoList.Add(toDoItem);
+                    db.SaveChanges();
+                }
             }
-
             return PartialView("_ToDoTable", GetToDoList());
         }
 
         public ActionResult Edit(int? id)
         {
             if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
             ToDoItem toDoItem = db.ToDoList.Find(id);
-
             if (toDoItem == null) return HttpNotFound();
-
             string currentUserId = User.Identity.GetUserId();
             ApplicationUser currentUser = db.Users.FirstOrDefault(x => x.Id == currentUserId);
-
             if (toDoItem.User != currentUser) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
             return View(toDoItem);
@@ -88,47 +82,35 @@ namespace ToDoApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Description,IsDone")] ToDoItem toDoItem)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && !string.IsNullOrWhiteSpace(toDoItem.Description))
             {
                 db.Entry(toDoItem).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return View(toDoItem);
         }
 
         [HttpPost]
         public ActionResult AJAXEdit(int? id, bool value)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             ToDoItem toDoItem = db.ToDoList.Find(id);
-
-            if (toDoItem == null)
-            {
-                return HttpNotFound();
-            }
-
+            if (toDoItem == null) return HttpNotFound();
             toDoItem.IsDone = value;
             db.Entry(toDoItem).State = EntityState.Modified;
             db.SaveChanges();
+
             return PartialView("_ToDoTable", GetToDoList());
         }
 
         public ActionResult Delete(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             ToDoItem toDoItem = db.ToDoList.Find(id);
-            if (toDoItem == null)
-            {
-                return HttpNotFound();
-            }
+            if (toDoItem == null) return HttpNotFound();
+
             return View(toDoItem);
         }
 
@@ -139,15 +121,13 @@ namespace ToDoApp.Controllers
             ToDoItem toDoItem = db.ToDoList.Find(id);
             db.ToDoList.Remove(toDoItem);
             db.SaveChanges();
+
             return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
-            {
-                db.Dispose();
-            }
+            if (disposing) db.Dispose(); 
             base.Dispose(disposing);
         }
     }
